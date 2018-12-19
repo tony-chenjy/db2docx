d823f065ec8b993ebe28b33d116fecf000c17e69
yu578707139 <tony.chenjy@foxmail.com>
2018-12-19 12:32:31 +0800

    c

--- main/Main.py
+++ main/Main.py
@@ -2,9 +2,9 @@ from db2docx import Mysql2Docx
 from db2docx import Oracle2Docx
 from db2docx import SqlServer2Docx
 
-m = Mysql2Docx()
-m.do(db_host='127.0.0.1', db_name='drklb', db_port=3307, db_user='root', db_pwd='root', file_name='数据字典_mysql',
-     file_path='/')
+# m = Mysql2Docx()
+# m.do(db_host='127.0.0.1', db_name='drklb', db_port=3307, db_user='root', db_pwd='root', file_name='数据字典_mysql',
+#      file_path='/')
 
 # o = Oracle2Docx()
 # o.do(db_host='192.168.0.71', db_name='drklb', db_port=3307, db_user='root', db_pwd='root', file_name='数据字典_oracle')
